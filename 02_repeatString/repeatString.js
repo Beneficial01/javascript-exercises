dcb2116d5e1fbbb751ba4697a606606df2af5159
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-02-25 18:41:58 +0100

    ✅ Solved ex2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(strToRepeat, numTimes) {
+    if(numTimes < 0)
+        // We are asked to return a string containing error in case of a negative factor
+        return 'ERROR';
+        
+    if(numTimes === 0 || strToRepeat.length === 0)
+        return '';
 
+    let i;
+    let strToAdd = strToRepeat;
+    for(i = 0; i < numTimes - 1; i++){
+        strToRepeat += strToAdd;
+    }
+    
+    return strToRepeat
 };
 
 // Do not edit below this line

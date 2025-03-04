fb1d416de98cbb794681f4fd056469be69e6ffb1
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-03-04 21:18:04 +0100

    ✅ finished ex4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    let s = new Set(rest);
+    let arrClean = [];
+    for(let x of arr){
+        if(!s.has(x)){
+            arrClean.push(x);
+        }
+    }
+    return arrClean
 };
 
 // Do not edit below this line

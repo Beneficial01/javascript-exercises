cc761118bc8df63cef575c5f15f3f7aa3fc11224
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-03-04 23:31:37 +0100

    ✅ completed ex5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+function isPositiveInteger(num){
+    return (Number.isInteger(num) && num >= 0)
+}
 
+const sumAll = function(a,b) {
+    if(isPositiveInteger(a) && isPositiveInteger(b)){
+        let max = Math.max(a,b);
+        let min = Math.min(a,b);
+        let sum = min;
+        for(let i = min + 1; i <= max ; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line

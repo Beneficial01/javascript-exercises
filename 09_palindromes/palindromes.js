4f48256cf1bfa7aee633a54cd89a52afb1df9423
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-03-05 11:24:23 +0100

    ✅ completed ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+function removePunctuation(str) {
+    return str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
 
+const palindromes = function (word) {
+    //We first clean the string
+    // Remove punctuation and spaces
+    word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, '');
+
+    word = word.toLowerCase();
+
+    for(let i = 0 ; i < Math.floor(word.length/2); i++ ){
+        if(word[i] !== word[word.length - 1 - i])
+            return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line

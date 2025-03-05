b114f12ad43b29e0414748d1a9b0c9a224b86e41
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-03-05 13:45:22 +0100

    ✅ completed ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function getAge(person){
+    if(person.yearOfDeath !== undefined)
+        return person.yearOfDeath - person.yearOfBirth;
+    
+    let currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    let ages = people.map(person => getAge(person));
+    return people[ages.indexOf(Math.max(...ages))];
 };
 
 // Do not edit below this line

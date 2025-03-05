114182efdeebdb47d0ae9c77875c07cc630bd054
Mahmoud Chairi <mahmoud.chairi@gmail.com>
2025-03-05 10:45:45 +0100

    ✅ completed ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+function roundDecimals(num, precision){
+  if(num == 0)
+    return 0;
+  let tmp = num*(10**(precision));
+  tmp = Math.round(tmp)
+  return tmp/(10**precision)
+}
+
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp-32)*(5/9);
+  return roundDecimals(convertedTemp,1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = temp*(9/5) + 32;
+  return roundDecimals(convertedTemp,1);
 };
 
 // Do not edit below this line
